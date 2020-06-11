c5e719cd68e808b8d42f8b2d238ab261716fd369
Rohit Bhandari <bhandarirohit080@gmail.com>
2020-06-11 12:35:34 +0530

    Update end.js

--- end.js
+++ end.js
@@ -5,7 +5,7 @@ const mostRecentScore = localStorage.getItem("mostRecentScore");
 
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-const MAX_HIGH_SCORES = 5;
+// console.log(highScores);
 
 finalScore.innerText = mostRecentScore;
 
@@ -27,5 +27,6 @@ saveHighScore = e => {
   highScores.splice(5);
 
   localStorage.setItem("highScores", JSON.stringify(highScores));
+//   console.log(highScores);
   window.location.assign('index.html');
 };
